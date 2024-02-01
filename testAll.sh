3e469d27f3d92790305b3f3e1f1a9b2c34552829
moriyaw <moriya@sodalabs.xyz>
2024-02-01 10:52:45 +0200

    Add a script that runs all tests (#4)

---
+++ testAll.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Function to print blue text
+print_blue() {
+    echo -e "\e[94m$1\e[0m"
+}
+
+print_blue "Running golang tests..."
+cd golang_cli
+go mod download
+go test -v ./...
+cd ..
+
+print_blue "Running javascript tests..."
+cd js
+npx mocha --require esm test.mjs
+cd ..
+
+print_blue "Running python tests..."
+cd python
+python3 -m unittest test.py
+
