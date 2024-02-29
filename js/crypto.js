92231f7bfdb3f07bd12c01525b4e13727da2d90b
moriyaw <moriya@sodalabs.xyz>
2024-02-29 19:17:55 +0200

    change prepareIt to get the signature after keccak

--- js/crypto.js
+++ js/crypto.js
@@ -167,14 +167,11 @@ export function sign(message, key) {
     return Buffer.concat([rBytes, sBytes, vByte]);
 }
 
-export function prepareIT(plaintext, userAesKey, sender, contract, func, signingKey) {
+export function prepareIT(plaintext, userAesKey, sender, contract, hashFunc, signingKey) {
 
     // Get the bytes of the sender, contract, and function signature
     const senderBytes = toBuffer(sender)
     const contractBytes = toBuffer(contract)
-    
-    const encodedABI = func.encodeABI();
-    const hashFunc = Buffer.from(encodedABI.substr(2, 8), 'hex');
     
     // Convert the plaintext to bytes
     const plaintextBytes = Buffer.alloc(8); // Allocate a buffer of size 8 bytes
